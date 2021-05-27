58d76ad446c1885403e898ac0dfe5039662da83e
Olzhas Samatov <70710100+olzhing@users.noreply.github.com>
2021-05-27 19:30:00 +0600

    Create cutstr.py

---
+++ cutstr.py
@@ -0,0 +1,8 @@
+s = input()
+print(s[2])  # third char
+print(s[-2]) # penultimate
+print(s[0:5]) # first 5 chars
+print(s[:-2]) # cut last 2 chars
+print(s[0::2]) # even chars
+print(s[::-1]) # reverse
+print(len(s)) # length
